1ef6bedfed4bd95ea74d3e46f1bd371311de4990
ShaajanBasheer <112340032+ShaajanBasheer@users.noreply.github.com>
2026-09-11 14:06:41 +0000

    Configure S3 remote Terraform state

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+backend "s3" {
+    bucket       = "shaajan-terraform-state-aws-web-hosting"
+    key          = "aws-web-application-hosting/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
